168bb8bb7128a329225da24c1b93e9a972dd3f1d
Mathew Rupp <13476020+MathewRupp@users.noreply.github.com>
2025-02-18 14:25:36 -0800

    Edited for Gitea

--- scripts/content.js
+++ scripts/content.js
@@ -1,98 +1,43 @@
-// only supports .kicad_sch, .kicad_pcb, .FCStd .step
-console.log("we're running");
+console.log("we're running on Gitea");
 
-const observer = new MutationObserver( list => {
-    const evt = new CustomEvent('dom-changed', {detail: list});
-    document.body.dispatchEvent(evt)
+const observer = new MutationObserver(list => {
+    const evt = new CustomEvent('dom-changed', { detail: list });
+    document.body.dispatchEvent(evt);
 });
-observer.observe(document.body, {attributes: true, childList: true, subtree: true});
+observer.observe(document.body, { attributes: true, childList: true, subtree: true });
 
-function insert(){
-    const mcadRegex = new RegExp('\.(FCStd|step|stp)$');
-    const ecadRegex = new RegExp('\.(kicad_pcb|kicad_sch|kicad_pro)$');
+function insert() {
+    const mcadRegex = /\.(FCStd|step|stp)$/;
+    const ecadRegex = /\.(kicad_pcb|kicad_sch|kicad_pro)$/;
 
-    const elements = document.querySelectorAll(".react-directory-filename-column a.Link--primary:not(.oshw-viewer)");
+    const elements = document.querySelectorAll(".file-list a:not(.oshw-viewer)");
 
-    // `document.querySelector` may return null if the selector doesn't match anything.
-    for(var i=0; i< elements.length; i++){
+    for (let i = 0; i < elements.length; i++) {
+        let fileLink = elements[i].href;
+        let fileName = elements[i].textContent.trim();
         
-        //document.querySelectorAll('.react-directory-filename-column a.Link--primary').filter(e => e.href.match(/\.(kicad_pcb|kicad_sch)$/)).forEach(e => e.insertAdjacentHTML('afterend', '<span> [<a target="_blank" href="https://kicanvas.org/?github='+encodeURIComponent(e.href)+'">KC</a>]</span>'))
         elements[i].classList.add("oshw-viewer");
-        //const ecadElements = elements[i].filter(e => e.href.match(/\.(kicad_pcb|kicad_sch)$/));
 
         const viewerButton = document.createElement("a");
         viewerButton.target = "_blank";
         viewerButton.style.display = "block";
-        
-        if(mcadRegex.test(elements[i].innerHTML)){
-            viewerButton.href = "https://3dviewer.net/#model=" + elements[i].href;
+
+        if (mcadRegex.test(fileName)) {
+            viewerButton.href = "https://3dviewer.net/#model=" + encodeURIComponent(fileLink);
             viewerButton.innerText = "View 3D Model";
-        }
-        else if(ecadRegex.test(elements[i].innerHTML)){
-            viewerButton.href = "https://kicanvas.org/?github=" + elements[i].href;
+        } else if (ecadRegex.test(fileName)) {
+            viewerButton.href = "https://kicanvas.org/?gitea=" + encodeURIComponent(fileLink);
             viewerButton.innerText = "View PCB";
-        }
-        else{
+        } else {
             continue;
         }
 
         elements[i].insertAdjacentElement("afterend", viewerButton);
-
-        // //if element has an innerhtml of one of the mcad filetypes
-        // console.log(elements[i].querySelector("div > h3 > div > a").innerHTML);
-        // console.log(mcadRegex.test(elements[i].querySelector("div > h3 > div > a").innerHTML));
-
-
-        // if(mcadRegex.test(elements[i].querySelector("div > h3 > div > a").innerHTML)){
-        //     //only in here if we had a match on regex
-        //     console.log("we got a match: ", elements[i].querySelector("div > h3 > div > a").innerHTML);
-
-            
-        //     const viewerButton = document.createElement("a");
-        //     viewerButton.href = "https://3dviewer.net/#model=" + elements[i].querySelector("div > h3 > div > a").href;
-        //     viewerButton.innerText = "View 3D Model";
-        //     viewerButton.target = "_blank";
-        //     viewerButton.classList = "oshw-viewer"
-
-        //     // const mcadImage = document.createElement("img");
-        //     // mcadImage.src = "https://cdn.shopify.com/s/files/1/0570/4256/7355/files/opulo-icon-gold-alpha.png?v=1689358351";
-        //     // mcadImage.style.height = "16px";
-        //     //mcadImage.src = chrome.extension.getURL('opulo.png');
-        //     //viewerButton.appendChild(mcadImage);
-
-        //     elements[i].querySelector("div > h3").insertAdjacentElement("afterend", viewerButton);
-            
-        // }
-
-        // //if element has innerhtml of ecad filetypes
-        // if(ecadRegex.test(elements[i].querySelector("div > h3 > div > a").innerHTML)){
-        //     //only in here if we had a match on regex
-        //     console.log("we got a match: ", elements[i].querySelector("div > h3 > div > a").innerHTML);
-
-        //     const viewerButton = document.createElement("a");
-        //     viewerButton.href = "https://kicanvas.org/?github=" + elements[i].querySelector("div > h3 > div > a").href;
-        //     viewerButton.innerText = "View PCB";
-        //     viewerButton.target = "_blank";
-
-        //     // const mcadImage = document.createElement("img");
-        //     // mcadImage.src = "https://cdn.shopify.com/s/files/1/0570/4256/7355/files/opulo-icon-gold-alpha.png?v=1689358351";
-        //     // mcadImage.style.height = "16px";
-        //     //mcadImage.src = chrome.extension.getURL('opulo.png');
-        //     //viewerButton.appendChild(mcadImage);
-
-        //     elements[i].querySelector("div > h3").insertAdjacentElement("afterend", viewerButton);
-            
-        // }
-
     }
-
-    //[...document.querySelectorAll('.react-directory-filename-column')].filter(e => e.querySelector('a.Link--primary[href$=\\.kicad_sch], a.Link--primary[href$=\\.kicad_pcb]'))
-            
-    
-    
 }
 
-document.body.addEventListener('dom-changed', e => {
-    // DOM changed, do stuff
+document.body.addEventListener('dom-changed', () => {
     insert();
 });
+
+insert();
